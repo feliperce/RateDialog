93be782e968cf79969551bafdb3c9ede20aca315
Felipe Rodrigues <felipe.celestino@ftd.com.br>
2018-11-16 16:46:48 -0200

    created dialog

--- sample/src/main/java/br/com/mobileti/sample/MainActivity.kt
+++ sample/src/main/java/br/com/mobileti/sample/MainActivity.kt
@@ -2,11 +2,14 @@ package br.com.mobileti.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.mobileti.ratedialog.RateDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RateDialog(this).create()
     }
 }
